0a8e7b3135ed11e22ad294e292c592aa516b4a12
Laurisson moura <89947116+laurisson@users.noreply.github.com>
2022-11-30 16:49:03 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".slide-characters", {
+  slidesPerView: 3.5,
+  spaceBetween: 19,
+  freeMode: true,
+  breakpoints:{
+    320: {
+      slidesPerView:1,
+    },
+
+    768: {
+      slidesPerView:2.1,
+    },
+
+    991: {
+      slidesPerView:2.8,
+    },
+
+    1200: {
+      slidesPerView:3.5,
+    },
+
+  }
+});
+
+AOS.init({
+  duration: 1000,
+  once: true,
+})
